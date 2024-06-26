1e17bfa89be151e29cc019546332d6e203aa4cbd
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2024-06-26 15:22:06 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -23,8 +23,26 @@ exports.createProduct = async (req, res) => {
     });
   }
 };
-
 exports.getProduct = async (req, res) => {
+  try {
+    const product = await Products.findById(req.params.id);
+    //   .populate("users")
+    //   .populate("products.product");
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({
+      message: "Product fetched successfully",
+      product,
+    });
+  } catch (e) {
+    return res.status(400).json({
+      message: e.message,
+    });
+  }
+};
+
+exports.getAllProduct = async (req, res) => {
   try {
     const products = await Products.find();
     if (!products) {
@@ -40,5 +58,42 @@ exports.getProduct = async (req, res) => {
     return res.status(400).json({
       error: e.message,
     });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const { name, price, description } = req.body;
+    const productId = req.params.id;
+
+    const updatedProduct = await Products.findByIdAndUpdate(
+      productId,
+      { name, price, description },
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({
+      message: "Product updated successfully",
+      product: updatedProduct,
+    });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const product = await Products.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted" });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
   }
 };
